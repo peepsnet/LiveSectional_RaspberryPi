211a8cb634bead1c5259065aa59651e3d0a5589f
peepsnet <peepsnet@yahoo.com>
2018-11-01 21:53:08 -0400

    Update install.sh

--- LiveSectional/install.sh
+++ LiveSectional/install.sh
@@ -2,19 +2,16 @@
 #This will install and correct a few library errors for the LiveSectional Script
 #WIP
 ## https://github.com/adafruit/Adafruit_Python_GPIO
-## https://github.com/adafruit/Adafruit_Python_WS2801
-## https://github.com/adafruit/Adafruit_Python_WS2801/pull/3 (need to modify file because of unaddresses typo)
-
-#sudo pip install rpi_ws281x
-#https://pypi.org/project/rpi_ws281x/
+## https://pypi.org/project/rpi_ws281x/
 
 parm="*"
 err="####"
 att="+++"
 
-#Set fonts for Help.
+#Set fonts
 BOLD=$(tput bold)
 STOT=$(tput smso)
+STOTX=$(tput rmso)
 DIM=$(tput dim)
 UNDR=$(tput smul)
 REV=$(tput rev)
@@ -26,71 +23,192 @@ WHITE=$(tput setaf 7)
 NORM=$(tput sgr0)
 NORMAL=$(tput sgr0)
 
-function HEAD {
+function HEAD() {
 	clear
+	echo "${REV}#######################################################################"
+	echo "#              ${NORM}LIVE SECTIONAL IINSTALL AND CONFIG TOOL${REV}                #"
 	echo "#######################################################################"
-	echo "#              LIVE SECTIONAL IINSTALL AND CONFIG TOOL                #"
-	echo "#######################################################################"
-	echo " "
+	echo "${NORM} "
 }
 
-function WELCOME {
+function WELCOME() {
 	echo ""
 	echo "Welcome to ..."
 	echo ""
 }
 
-function STARTINSTALL {
-	HEAD
-	echo " "
-	echo 
-}
-
-function INSTALL_LIVESECTIONAL {
+function INSTALL_LIVESECTIONAL() {
+	echo "${GREEN}${BOLD}=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=${NORM}"
+	echo "${BOLD}Getting Live Sectional WS2811 files from the github...${NORM}"
 	cd ~/
-	sudo mkdir LiveSectional
+	mkdir LiveSectional
 	cd LiveSectional
 	#wget https://raw.githubusercontent.com/peepsnet/LiveSectional_RaspberryPi/master/LiveSectional/LiveSectional.py
 	#wget https://raw.githubusercontent.com/peepsnet/LiveSectional_RaspberryPi/master/LiveSectional/airports.txt
 	#wget https://raw.githubusercontent.com/peepsnet/LiveSectional_RaspberryPi/master/LiveSectional/config.txt
-	#wget https://raw.githubusercontent.com/peepsnet/LiveSectional_RaspberryPi/master/LiveSectional/pishutdown.py
-}
-
-finction INSTALL_PIP_RPI_WS281X {
-	#sudo pip install rpi_ws281x
-}
-
-function INSTALL_PIP_GPIO {
-	#sudo pip install RPi.GPIO
+	#wget https://raw.githubusercontent.com/peepsnet/LiveSectional_RaspberryPi/master/LiveSectional/pipowerbtn.py
+	#wget https://raw.githubusercontent.com/peepsnet/LiveSectional_RaspberryPi/master/LiveSectional/pipowerbtn.service
+	#wget https://raw.githubusercontent.com/peepsnet/LiveSectional_RaspberryPi/master/LiveSectional/livesectional.service
+	#wget https://raw.githubusercontent.com/peepsnet/LiveSectional_RaspberryPi/master/LiveSectional/single.py
+	#wget https://raw.githubusercontent.com/peepsnet/LiveSectional_RaspberryPi/master/LiveSectional/test.py
+	#sudo chmod +x LiveSectional.py
+	#sudo chmod +x single.py
+	#sudo chmod +x test.py
+	
 	echo ""
 }
 
-function EDIT_RC_LOCAL {
-	## edit rc.local to call /usr/bin/python ~/LiveSectional/pishutdown.py
-	## edit rc.local to call /usr/bin/python ~/LiveSectional/LiveSectional.py startup
+function INSTALL_PIP_RPI_WS281X() {
+	echo "${GREEN}${BOLD}=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=${NORM}"
+	echo "${BOLD}Installing the RPI_WS281x python library using PIP...${NORM}"
+	#sudo sudo pip install rpi_ws281x
 	echo ""
 }
 
-function ADD_CRON {
-	## Add CronJob
+function INSTALL_PIP_GPIO() {
+	echo "${GREEN}${BOLD}=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=${NORM}"
+	echo "${BOLD}Installing the RPI.GPIO python library using PIP...${NORM}"
+	#sudo sudo pip install RPi.GPIO
 	echo ""
 }
 
-function INSTALL_APTGET_EXTRAS {
+function CREATE_SYSTEMD_ENTRIES() {
+	echo "${GREEN}${BOLD}=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=${NORM}"
+	echo "${BOLD}Copying .service files to /etc/systemd/system/ and updating the systemctrl...${NORM}"
+	#cp  ~/LiveSectional/pipowerbtn.service /etc/systemd/system/
+	#cp  ~/LiveSectional/livesectional.service /etc/systemd/system/
+	#sudo systemctl daemon-reload
+	#sudo systemctl enable livesectional.service --now.
+	#sudo systemctl enable pipowerbtn.service --now.
+	echo ""
+}
+
+function ADD_CRON() {
+	echo "Choose your METAR update interval:"
+	choices=( 'Every 15 min' 'Every 20 min' 'Every 30 min' 'Top of every hour' 'Every X min')
+		select choice in "${choices[@]}"; do
+			[[ -n $choice ]] || { echo "Invalid choice." >&2; continue; }
+			case $choice in
+				'Every 15 min')
+					echo "Setting schedule to every 15 min"
+					;;
+				'Every 20 min')
+					echo "Setting schedule to every 20 min"
+					;;
+				'Every 30 min')
+					echo "Setting schedule to every 30 min"
+					;;
+				'Top of every hour')
+					echo "Setting schedule to top of every hour"
+					;;
+				'Every X min')
+					echo "Setting schedule to every X min"
+					;;
+				'Exit')
+					exit 0
+				;;
+				exit)
+				exit 0
+			esac
+		break
+	done
+	echo "${GREEN}${BOLD}=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=${NORM}"
+	echo "${BOLD}Adding cronjob...${NORM}"
+	#write out current crontab
+	crontab -l > mycron.tmp
+	#echo new cron into cron file
+	echo "*/15 * * * * sudo /usr/bin/python ~/LiveSectional/LiveSectional.py" >> mycron
+	#install new cron file
+	crontab mycron
+	rm mycron.tmp
+	echo ""
+}
+
+function INSTALL_APTGET_EXTRAS() {
+	echo "${GREEN}${BOLD}=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=${NORM}"
+	echo "${BOLD}Installing APT-GET packages..."
 	sudo apt-get install build-essential python-pip python-dev python-smbus git -y
+	echo ""
 }
 
-function APTGETUPDATE {
+function APTGETUPDATE() {
+	echo "${GREEN}${BOLD}=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=${NORM}"
+	echo "${BOLD}Lets start by gathering the lastest packages...${NORM}"
 	sudo apt-get update
+	echo ""
 }
 
-function APTGETUPGRADE {
+function APTGETUPGRADE() {
+	echo "${GREEN}${BOLD}=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=${NORM}"
+	echo "${BOLD}Now we need to update your Raspberry Pi to the latest software...${NORM}"
 	sudo apt-get upgrade -y
+	echo ""
 }
 
+function REBOOTSYS() {
+	echo "REBOOTING!!!"
+	#sleep 5s; sudo shutdown -r now
+	echo ""
+}
+
+function CHECKINTERNET() {
+	echo "Checking for internet connection..."
+	wget -q --spider http://google.com
+	if [ $? -eq 0 ]; then
+		echo "${GREEN}Internet connection detected..${NORM}"
+		return 0
+	else
+		echo "${RED}${err} No internet found. ${err}"
+		echo "${BOLD}This script requires an internet connection. Files must be downloaded from the internet."
+		echo "Please refer to the readme..md at: https://github.com/peepsnet/LiveSectional_RaspberryPi${NORM}"
+		echo "Exiting..."
+		return 1
+	fi
+}
+
+function ROOTCHECK() {
+	echo "${BOLD}Checking if script is run as root...${NORM}"
+	if [ $(whoami) != 'root' ]; then
+		echo "${BOLD}${RED}This script must be executed as root${WHITE}${NORM}"
+		echo "${BOLD}Please run as root: ${STOT}sudo ./install.sh${NORM}"
+		echo "Exiting..."
+		exit 1
+	else
+		echo "${GREEN}Root Check passed...${NORM}"
+		echo "${GREEN}Continuing...${NORM}"
+	fi
+}
+
+function COMPLETEDINSTALL() {
+	HEAD
+	echo "Basic instalation and configuration of LiveSectional is complete..."
+	echo "The next step is to set up your LEDs and Airports."
+	echo ""
+	echo "Prese enter/return to continue..."
+	read -p "" readDamKey
+}
+
+function EDITLEDSAIRPORTS() {
+	echo ""
+	echo "Description on how to edit the airports.txt file"
+	echo "Prese enter/return to begin editing the airport list..."
+	read -p "" readDamKey
+	nano airports.txt
+}
+###################################
+###################################
 ## Start of work
+###################################
+###################################
+
 clear
 
+HEAD
+#ROOTCHECK
+if ! CHECKINTERNET; then
+	exit 0
+fi
+sleep 2s
 HEAD
 WELCOME
 
@@ -98,48 +216,50 @@ choices=( 'Change Settings' 'Install and Configure' 'Exit' )
 select choice in "${choices[@]}"; do
 	[[ -n $choice ]] || { echo "Invalid choice." >&2; continue; }
 	case $choice in
-    	'Change Settings')
+		'Change Settings')
 			echo "Change Settings"
-    	    ;;
-    	'Install and Configure')
-			echo "Install and Config"
+			;;
+		'Install and Configure')
+			HEAD
+			echo "Installing and configuring files"
 			APTGETUPDATE
+			APTGETUPGRADE
 			INSTALL_APTGET_EXTRAS
+			INSTALL_LIVESECTIONAL
 			INSTALL_PIP_RPI_WS281X
-			#INSTALL_PIP_GPIO
-			#EDIT_RC_LOCAL
-			#ADD_CRON
+			INSTALL_PIP_GPIO
+			CREATE_SYSTEMD_ENTRIES
+			ADD_CRON
+			sleep 5s
+			COMPLETEDINSTALL
+			EDITLEDSAIRPORTS
+			sleep 5s
+			REBOOTSYS
 			;;
 		'Exit')
 			exit 0
 		;;
 		exit)
+		exit 0
+		esac
+	break
+	done
 
-    exit 0
-  	esac
-break
-done
+#################################
+##  Useful code storage
+#################################
 
-if [ $(whoami) != 'root' ]; then
-	echo "${BOLD}${RED}This script must be executed as root, exiting...${WHITE}${NORMAL}"
-	echo "${BOLD}${RED}USAGE${WHITE}${NORMAL}"
-	exit 1
-fi
+# Screen Params
+#_COLUMNS=$(tput cols)
+#_LINES=$(tput lines)
+#_MESSAGE="HELLO AND WELCOME\nPRESS ANY KEY TO CONTINUE"
+#x=$(( $_LINES / 2 ))
+#y=$(( ( $_COLUMNS - ${#_MESSAGE} )  / 2 ))
+#tput clear
+#tput cup $x $y
+#read -p "" readDamKey 
 
-
-
-
-#cd ~/
-#git clone https://github.com/adafruit/Adafruit_Python_GPIO.git
-#cd Adafruit_Python_GPIO
-#sudo /usr/bin/python setup.py install
-#sudo pip install adafruit-ws2801
-#sudo rm -r Adafruit_Python_GPIO
-
-## edit /home/pi/.local/lib/python2.7/site-packages/Adafruit_WS2801/WS2801.py line 124
-## self.set_pixel_rgb(i, r, g, b)
-
-## Add CronJob
-
-## edit rc.local to call /usr/bin/python ~/LiveSectional/pishutdown.py
-## edit rc.local to call /usr/bin/python ~/LiveSectional/LiveSectional.py startup
+# Start cleaning up our screen...
+#tput clear
+#tput sgr0
+#tput rc
